488b40c145c24d95b22ac52a447c33d2f3b5e3a2
yheun03 <eyh208@gmail.com>
2021-11-27 09:49:30 +0900

    backup

---
+++ resources/scripts/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $.datepicker.setDefaults({
+        dateFormat: 'yymmdd',
+        prevText: '이전 달',
+        nextText: '다음 달',
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '년'
+    });
+
+    var fileTarget = $('.ipt-select-file input[type=file]');
+    fileTarget.on('change', function(){ // 값이 변경되면
+        if(window.FileReader){ // modern browser
+            var filename = $(this)[0].files[0].name;
+        } else { // old IE
+                var filename = $(this).val().split('/').pop().split('\\').pop(); // 파일명만 추출
+        } // 추출한 파일명 삽입
+        $(this).siblings('.upload-name').text(filename);
+    });
+    var imgTarget = $('.preview-image input[type="file"]');
+    imgTarget.on('change', function(){
+        var parent = $(this).parent().find(".img-wrap");
+        parent.children('.upload-display').remove();
+        if(window.FileReader){ //image 파일만
+            if (!$(this)[0].files[0].type.match(/image\//)) return;
+            var reader = new FileReader();
+            reader.onload = function(e){
+                var src = e.target.result;
+                parent.append('<div class="upload-display"><div class="upload-thumb-wrap"><img src="'+src+'" class="upload-thumb"></div></div>');
+            }
+            reader.readAsDataURL($(this)[0].files[0]);
+        } else { $(this)[0].select(); $(this)[0].blur();
+            var imgSrc = document.selection.createRange().text;
+            parent.append('<div class="upload-display"><div class="upload-thumb-wrap"><img class="upload-thumb"></div></div>');
+            var img = $(this).siblings('.upload-display').find('img');
+            img[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enable='true',sizingMethod='scale',src=\""+imgSrc+"\")";
+        }
+    });
+
+})
