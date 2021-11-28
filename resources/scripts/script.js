8770e9e3df169489307cb67661147f78ae31aad6
yheun03 <eyh208@gmail.com>
2021-11-28 22:29:01 +0900

    아이콘 추가 및 disabled 커서 수정, datepicker right left arrow 아이콘 추가 및 수정

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $.datepicker.setDefaults({
-        dateFormat: 'yymmdd',
+        dateFormat: 'yy-mm-dd',
         prevText: '이전 달',
         nextText: '다음 달',
         monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
