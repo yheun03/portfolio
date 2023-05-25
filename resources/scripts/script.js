eb849e796e5bbcdc1823aa7cbffc673a9a8d7dc1
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2023-05-25 23:20:43 +0900

    익일 회사에서 할 내용 커밋

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,7 +1,9 @@
 $(document).ready(()=>{
+    // NOTE: .area-history 내부의 경력 자동 세팅
+    // 최초개발일 : 20230525
+    // 수  정  일 : 20230525
     let year = new Date().getFullYear(); // 년도
     let month = new Date().getMonth() + 1  // 월
-
     const oldDate = new Date('2020-06');
     const newDate = new Date(year +"-"+ month);
     let diff = Math.abs((newDate.getFullYear() - oldDate.getFullYear())*12 + (newDate.getMonth() - oldDate.getMonth())) + 1;
@@ -21,4 +23,8 @@ $(document).ready(()=>{
     }else{
         target.attr("title", "단순 입퇴사일 계산 : "+year_2+"년 "+month_2+"개월차")
     }
+
+    // NOTE: .area-project 내부의 기능
+    // 최초개발일 : 20230525
+    // 수  정  일 : 20230525
 })
