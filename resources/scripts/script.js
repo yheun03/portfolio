ceae750e681e4c3f7b3c99aac253e06449de468e
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2023-06-26 19:44:17 +0900

    html 속성 ID 변경했는데 css 수정 안 해서 나는 에러 수정

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -31,17 +31,17 @@ $(document).ready(()=>{
     showProject.change(()=>{
         console.log(this.value)
     })
-    // let project_list = $(".wrap-dscpt > div > div");
-    // for(let i = 1; i < project_list.length + 1; i++){
-    //     $(".progress li:nth-child("+ i +") a").click(()=>{
-    //         $(".screen > div > img:nth-child("+i+")").show().siblings().hide();
-    //         $(".wrap-dscpt > div > div:nth-child("+i+")").show().siblings().hide();
-    //     })
-    // }
+    let project_list = $(".wrap-dscpt > div > div");
+    for(let i = 1; i < project_list.length + 1; i++){
+        $(".progress li:nth-child("+ i +") a").click(()=>{
+            $(".screen > div > img:nth-child("+i+")").show().siblings().hide();
+            $(".wrap-dscpt > div > div:nth-child("+i+")").show().siblings().hide();
+        })
+    }
 
     $("a[href='#']").click(()=>{
         return false;
     })
 
-    alert("모바일 반응형과 애니메이션, 기능은 개발 중입니다.\n기다려 주셔서 감사합니다.")
+    // alert("모바일 반응형과 애니메이션, 기능은 개발 중입니다.\n기다려 주셔서 감사합니다.")
 })
