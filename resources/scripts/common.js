d1d2938add206cf9a05c02e9f811c911532ac143
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2023-08-14 21:55:00 +0900

    layout.scss 리팩토링 완성 - 반응형 리팩토링 진행예정

--- resources/scripts/common.js
+++ resources/scripts/common.js
@@ -7,21 +7,13 @@ $(document).ready(()=>{
     const oldDate = new Date('2020-06');
     const newDate = new Date(year +"-"+ month);
     let diff = Math.abs((newDate.getFullYear() - oldDate.getFullYear())*12 + (newDate.getMonth() - oldDate.getMonth())) + 1;
-    let diff_2 = diff + 13
     year = parseInt(diff/12);
     month = diff%12;
-    let year_2 = parseInt(diff_2/12);
-    let month_2 = diff_2%12;
     let target = $("[data-hstryFg='career'] b span")
     if(month == 0){
         target.html(year+"년차")
     }else{
         target.html(year+"년 " + month+"개월차")
-    }
-    if(month_2 == 0){
-        target.attr("title", "단순 입퇴사일 계산 : "+year_2+"년차")
-    }else{
-        target.attr("title", "단순 입퇴사일 계산 : "+year_2+"년 "+month_2+"개월차")
     }
 
     // NOTE: .area-project 내부의 기능  -- 삭제
