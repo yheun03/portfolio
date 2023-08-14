04931233ac8402a413c240a65f8fe5967047dc7f
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2023-08-14 23:14:47 +0900

    배포용 alert push

--- resources/scripts/common.js
+++ resources/scripts/common.js
@@ -1,7 +1,7 @@
 $(document).ready(()=>{
     // NOTE: .area-history 내부의 경력 자동 세팅
     // 최초개발일 : 20230525
-    // 수  정  일 : 20230530
+    // 수  정  일 : 20230814
     let year = new Date().getFullYear(); // 년도
     let month = new Date().getMonth() + 1  // 월
     const oldDate = new Date('2020-06');
@@ -19,7 +19,7 @@ $(document).ready(()=>{
     // NOTE: .area-project 내부의 기능  -- 삭제
     // 최초개발일 : 20230525
     // 수  정  일 : 20230627
-    // 최종수정일 : 20230810
+    // 최종수정일 : 20230814
     $(".project-list").slick({
         fade: true,
         centerMode: true,
@@ -31,14 +31,12 @@ $(document).ready(()=>{
     var dom = $(".nav");
     dom.detach();
     $(".project-list").prepend(dom)
-    // $(".slick-list").before($(".nav"))
-    // $(".prev").before($(".slick-dots"))
     
     $("a[href='#']").click(()=>{
         return false;
     })
 
-    // alert("모바일 반응형과 애니메이션은 개발 중입니다.\n기다려 주셔서 감사합니다.")
+    alert("모바일 반응형과 애니메이션은 개발 중입니다.\n기다려 주셔서 감사합니다.")
 
 
 
