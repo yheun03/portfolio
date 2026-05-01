ec52921891f2314c2dd40548c1a26f901b2de59a
Nyoung_Ni <eyh208@gmail.com>
2026-05-01 18:38:04 +0900

    Remove: 삭제된 Nuxt 관련 구성 파일 및 타입 정의로 인해 프로젝트의 불필요한 파일을 정리합니다. 이로써 코드베이스를 간소화하고 유지보수성을 향상시킵니다.

--- .nuxt/imports.d.ts
+++ .nuxt/imports.d.ts
@@ -33,4 +33,3 @@ export { defineLazyHydrationComponent } from '#app/composables/lazy-hydration';
 export { useTheme } from '../core/composables/useTheme';
 export { usePortfolioStore } from '../core/stores/usePortfolioStore';
 export { defineStore, acceptHMRUpdate, usePinia, storeToRefs } from '../node_modules/@pinia/nuxt/dist/runtime/composables';
-export { useNuxtDevTools } from '../node_modules/@nuxt/devtools/dist/runtime/use-nuxt-devtools';
--- .nuxt/types/build.d.ts
+++ .nuxt/types/build.d.ts
@@ -12,8 +12,6 @@ declare module "#build/paths.mjs";
 declare module "#build/root-component.mjs";
 declare module "#build/plugins.server.mjs";
 declare module "#build/test-component-wrapper.mjs";
-declare module "#build/devtools/settings.mjs";
-declare module "#build/runtime.vue-devtools-client.IpheNOGo9iangp7PBD0Q8zIUiMEw23iwFRvS3YVxk3Q.js";
 declare module "#build/routes.mjs";
 declare module "#build/pages.mjs";
 declare module "#build/router.options.mjs";
