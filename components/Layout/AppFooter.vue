7358ffe403dfcec6afb891209b84dde57683fcde
Nyoung_Ni <eyh208@gmail.com>
2026-05-05 16:32:53 +0900

    Enhance: 새로운 애니메이션 효과와 스타일을 추가하여 사용자 인터페이스를 개선합니다. accent-emoji 클래스와 관련된 애니메이션을 정의하고, 헤더 및 푸터에 새로운 요소를 추가하여 시각적 매력을 높입니다. 또한, 섹션 제목에 스파크 효과를 통합하여 디자인 일관성을 강화합니다.

--- components/Layout/AppFooter.vue
+++ components/Layout/AppFooter.vue
@@ -1,5 +1,8 @@
 <template>
     <footer class="app-footer">
+        <p class="app-footer__emoji-line" aria-hidden="true">
+            <span class="accent-emoji accent-emoji--soft">✦</span>
+        </p>
         <p>{{ text }}</p>
     </footer>
 </template>
