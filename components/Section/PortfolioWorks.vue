7352a85b107826c30d5ad1b81d08d5602a272db7
Nyoung_Ni <eyh208@gmail.com>
2026-05-05 15:48:47 +0900

    Refactor: 하이라이트 카드의 포트폴리오 스타일을 업데이트하여 유연한 크기를 사용하고 텔레포트를 통해 모달 구조를 개선하여 접근성과 사용자 경험을 향상시킵니다. 작업 항목을 수정된 역할, 기술 및 업적으로 업데이트하여 명확성과 일관성을 유지합니다.

--- components/Section/PortfolioWorks.vue
+++ components/Section/PortfolioWorks.vue
@@ -25,38 +25,40 @@
                 @select="openWork(work)" />
         </div>
 
-        <div v-if="activeWork" class="works__modal" role="dialog" aria-modal="true" aria-labelledby="works-modal-title"
-            aria-describedby="works-modal-description" @click.self="closeModal">
-            <div ref="modalCardRef" class="works__modal-content" @click.stop>
-                <BaseCard :animate="false" class="works__modal-card">
-                    <div class="works__modal-head">
-                        <h3 id="works-modal-title">{{ pick(activeWork.title) }}</h3>
-                        <button ref="closeButtonRef" type="button"
-                            class="base-button base-button--ghost works__modal-close"
-                            :aria-label="labels.closeModalAria" @click="closeModal">
-                            {{ labels.close }}
-                        </button>
-                    </div>
-                    <p id="works-modal-description" class="works__meta">{{ activeWork.period }} · {{
-                        pick(activeWork.type) }}</p>
-                    <p class="works__role"><strong>{{ labels.role }}:</strong> {{ pick(activeWork.role) }}</p>
-                    <p class="works__intro">{{ pick(activeWork.introduction) }}</p>
-                    <p class="works__section-title"><strong>{{ labels.contributions }}</strong></p>
-                    <ul>
-                        <li v-for="item in activeWork.myWorks" :key="pick(item)">{{ pick(item) }}</li>
-                    </ul>
-                    <p v-if="activeWork.achievements.length" class="works__section-title"><strong>{{ labels.results
-                            }}</strong></p>
-                    <ul v-if="activeWork.achievements.length">
-                        <li v-for="item in activeWork.achievements" :key="pick(item)">{{ pick(item) }}</li>
-                    </ul>
-                    <p class="works__section-title"><strong>{{ labels.points }}</strong></p>
-                    <ul>
-                        <li v-for="item in activeWork.points" :key="pick(item)">{{ pick(item) }}</li>
-                    </ul>
-                </BaseCard>
+        <Teleport to="body">
+            <div v-if="activeWork" class="works__modal" role="dialog" aria-modal="true" aria-labelledby="works-modal-title"
+                aria-describedby="works-modal-description" @click.self="closeModal">
+                <div ref="modalCardRef" class="works__modal-content" @click.stop>
+                    <BaseCard :animate="false" class="works__modal-card">
+                        <div class="works__modal-head">
+                            <h3 id="works-modal-title">{{ pick(activeWork.title) }}</h3>
+                            <button ref="closeButtonRef" type="button"
+                                class="base-button base-button--ghost works__modal-close"
+                                :aria-label="labels.closeModalAria" @click="closeModal">
+                                {{ labels.close }}
+                            </button>
+                        </div>
+                        <p id="works-modal-description" class="works__meta">{{ activeWork.period }} · {{
+                            pick(activeWork.type) }}</p>
+                        <p class="works__role"><strong>{{ labels.role }}:</strong> {{ pick(activeWork.role) }}</p>
+                        <p class="works__intro">{{ pick(activeWork.introduction) }}</p>
+                        <p class="works__section-title"><strong>{{ labels.contributions }}</strong></p>
+                        <ul>
+                            <li v-for="item in activeWork.myWorks" :key="pick(item)">{{ pick(item) }}</li>
+                        </ul>
+                        <p v-if="activeWork.achievements.length" class="works__section-title"><strong>{{ labels.results
+                                }}</strong></p>
+                        <ul v-if="activeWork.achievements.length">
+                            <li v-for="item in activeWork.achievements" :key="pick(item)">{{ pick(item) }}</li>
+                        </ul>
+                        <p class="works__section-title"><strong>{{ labels.points }}</strong></p>
+                        <ul>
+                            <li v-for="item in activeWork.points" :key="pick(item)">{{ pick(item) }}</li>
+                        </ul>
+                    </BaseCard>
+                </div>
             </div>
-        </div>
+        </Teleport>
     </section>
 </template>
 
