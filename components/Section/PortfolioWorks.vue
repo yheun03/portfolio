aa6ecc565b48cb9db0e7e6d9b13123e7dafebf93
Nyoung_Ni <eyh208@gmail.com>
2026-05-05 16:20:49 +0900

    Refactor: SCSS 파일에서 불필요한 새 줄을 제거하고 스타일 구조를 최적화하여 가독성을 향상시키고 일관성을 유지합니다. 포트폴리오 섹션의 클래스 이름을 업데이트하여 명확성을 높이고, 헤더 및 버튼 스타일을 개선하여 사용자 경험을 향상시킵니다.

--- components/Section/PortfolioWorks.vue
+++ components/Section/PortfolioWorks.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="works" class="section">
+    <section id="works" class="section section--works">
         <BaseSectionTitle :eyebrow="t('nav.works')" :title="t('works.title')" />
         <p class="works__lead" data-animate>
             {{ locale === "ko" ? `총 ${filteredWorks.length}개의 프로젝트를 인터랙티브 카드로 확인할 수 있습니다.` : `Explore
