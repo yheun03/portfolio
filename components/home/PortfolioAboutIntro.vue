227609d35419a6b65993ffba22334f2ff1560ae8
Nyoung_Ni <eyh208@gmail.com>
2026-08-17 12:13:25 +0900

    Refactor: SCSS 구조 및 컴포넌트 업데이트
    - SCSS 파일에서 스타일 구조를 개선하고, 불필요한 코드를 제거하여 가독성을 높였습니다.
    - 'PortfolioWhy' 컴포넌트를 'PortfolioAboutIntro' 및 'PortfolioWhyList'로 분리하여 구성 요소의 명확성을 향상시켰습니다.
    - 미디어 쿼리를 추가하여 반응형 디자인을 강화하고, 레이아웃 일관성을 개선했습니다.
    - 관련된 SCSS 클래스 이름을 업데이트하여 코드의 일관성을 유지했습니다.

---
+++ components/home/PortfolioAboutIntro.vue
@@ -0,0 +1,20 @@
+<template>
+    <HomeSection id="about" section-class="section--about-intro" title-id="section-about-title">
+        <header class="about__intro" data-animate>
+            <p class="section-title__eyebrow">{{ t("nav.about") }}</p>
+
+            <h2 id="section-about-title" class="about__display">
+                <span class="about__display-line" style="--line-index: 0">{{ t("about.displayLine1") }}</span>
+                <span class="about__display-line about__display-line--accent" style="--line-index: 1">{{
+                    t("about.displayLine2") }}</span>
+                <span class="about__display-line" style="--line-index: 2">{{ t("about.displayLine3") }}</span>
+            </h2>
+
+            <p class="about__lead">{{ t("about.tagline") }}</p>
+        </header>
+    </HomeSection>
+</template>
+
+<script setup lang="ts">
+const { t } = useLocale();
+</script>
