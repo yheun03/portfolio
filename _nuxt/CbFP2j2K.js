e3353f70b9dcaabc7a06b2531538e9d6cde66f7e
Nyoung_Ni <eyh208@gmail.com>
2026-09-09 22:59:15 +0900

    Updates

--- _nuxt/CbFP2j2K.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./CBYkRQ9a.js";import{d as t,z as s,h as n,e as p,o as c}from"./DHBVpIO-.js";import{u as m}from"./D_dZ7Mmi.js";import"./CYcAnFVf.js";const w=t({__name:"[id]",setup(_){const o=s(),e=m("personal",o.params.id);return(l,u)=>{const r=a;return c(),n(r,{variant:"personal",work:p(e)},null,8,["work"])}}});export{w as default};
