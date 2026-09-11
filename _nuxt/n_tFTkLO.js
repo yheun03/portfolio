b1d9189412fde2438dfe58c5ef33c5267b662066
Nyoung_Ni <eyh208@gmail.com>
2026-09-12 00:44:57 +0900

    Updates

--- _nuxt/n_tFTkLO.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./DlSbdZG7.js";import{d as t,z as s,h as n,e as p,o as c}from"./BkufSyjU.js";import{u as m}from"./2CHYje-B.js";import"./y_mAxR0N.js";const w=t({__name:"[id]",setup(_){const o=s(),e=m("personal",o.params.id);return(l,u)=>{const r=a;return c(),n(r,{variant:"personal",work:p(e)},null,8,["work"])}}});export{w as default};
